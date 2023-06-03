d48b7cc928c259e919cfa444fea95a4d5bc03bb7
NadavSofer <nadavsof5@gmail.com>
2023-06-03 19:00:38 +0300

    added random palette generation

--- palettle_project_top/accounts/static/JS/random.js
+++ palettle_project_top/accounts/static/JS/random.js
@@ -1,10 +1,3 @@
-
-
-//http://127.0.0.1:8000/api/endpoint/
-
-
-
-
 (function makePalette() {
     let makePalette = document.getElementById('makePalette-container');
     for (let i = 0; i < 5; i++) {
@@ -25,9 +18,61 @@
             box.style.backgroundColor = input.value;
         })
 
-
         makePalette_container.appendChild(box);
         makePalette_container.appendChild(input);
         makePalette.appendChild(makePalette_container);
     }
 })()
+
+const getAPI = async ()=>{
+    try {
+        let url = await fetch('http://127.0.0.1:8000/api/endpoint/');
+        let response = await url.json();
+        return response
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const renderAPI = (arr) => {
+    let randomPalette = document.getElementById('randomPalette-container');
+    randomPalette.innerHTML = ''
+    delete arr.pk
+    for (const color of Object.values(arr)) {
+        let color_container = document.createElement('div');
+        color_container.classList.add('color-container');
+
+        let box = document.createElement('div');
+        box.classList.add('box');
+        box.style.width = '5rem';
+        box.style.height = '5rem';
+        box.style.backgroundColor = color;
+
+        let p = document.createElement('p');
+        p.innerText = color
+
+        color_container.appendChild(box)
+        color_container.appendChild(p)
+        randomPalette.appendChild(color_container)
+        console.log(color);
+    }
+}
+
+getAPI()
+.then((res)=> {
+    let random_num = Math.floor(Math.random() * res.length);
+    renderAPI(res[random_num]);
+})
+
+let randomGenerateButton = document.getElementById('random-generate');
+randomGenerateButton.innerText = 'generate'
+
+randomGenerateButton.addEventListener('click', ()=>{
+    let randomPalette = document.getElementById('randomPalette-container');
+    randomPalette.innerHTML = '<i class="fas fa-spinner fa-pulse"></i>'
+    getAPI()
+    .then((res)=> {
+        let random_num = Math.floor(Math.random() * res.length);
+        renderAPI(res[random_num]);
+})
+})
